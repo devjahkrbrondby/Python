0739a014bab246638538e1d32709ba3eda88208e
jhkcphdata <jhk@copenhagendata.com>
2022-01-03 10:11:03 +0100

    ...

--- PythonMigrateDataFromSqlToSql.py
+++ PythonMigrateDataFromSqlToSql.py
@@ -31,6 +31,12 @@ sql_query = pd.read_sql_query('''
                               '''
                               ,conn)
 
+
+# conn = odbc.connect('Driver={SQL Server};'
+#                       'SERVER=LAPTOP-3EFU92FT\MSSQLSERVER01;'
+#                       'Database=tSQLt_Database;'
+#                       'Trusted_Connection=yes;')
+
 df = pd.DataFrame(sql_query)
 print(df)
 # Insert Dataframe into SQL Server:
@@ -39,8 +45,8 @@ cursor.fast_executemany = True
 for row_count in range(0, df.shape[0]):
    chunk = df.iloc[row_count:row_count + 1,:].values.tolist()
    tuple_of_tuples = tuple(tuple(x) for x in chunk)
-   cursor.executemany("insert into dbo.Lag1_Table4" + " (EmployeeId,Education,Subjects,LenghtOfEducation) values (?,?,?,?)",tuple_of_tuples)
-   conn.commit()
+   cursor.executemany("insert into dbo.Lag1_Table1" + " (EmployeeId,Education,Subjects,LenghtOfEducation) values (?,?,?,?)",tuple_of_tuples)
+conn.commit()
 cursor.close()
 
 
